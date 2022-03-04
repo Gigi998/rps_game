6124f15032c03f1746226f52120107d4bbbaf8cf
Luigi <luigi.drnasin12@gmail.com>
2022-03-04 09:17:02 +0100

    One round game

--- javascript.js
+++ javascript.js
@@ -1,12 +1,30 @@
 function computerplay(){
-    var computer = ["Rock", "Paper", "Scissors"];
-    var computerChoice = player[Math.floor(Math.random() * player.length)];
-    return computerChoice;
+    var computer = ["rock", "paper", "scissors"];
+    var computerMove = computer[Math.floor(Math.random() * computer.length)];
+    return computerMove;
 }
 
-function game (playerSelection, computerSelection) {
-    
+function playRound (playerSelection, computerSelection) {
+
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        return("You lose! Paper beats Rock");
+    }else if (playerSelection == "rock" && computerSelection == "scissors"){
+        return("You win! Rock beats scissors");
+    }else if (playerSelection == "paper" && computerSelection == "scissors"){
+        return("You lose! Scissors beats paper");
+    }else if (playerSelection == "paper" && computerSelection == "rock"){
+        return("You win! Paper beats rock");
+    }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        return("You win!" + playerSelection + " beats " + computerSelection);
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return("You lose! Rock beats scissors");
+    }else {
+        return("It is a tie");
+    }
 }
 
-
+const playerSelection1 = "rock";
+const playerSelection = playerSelection1.toLowerCase();
+const computerSelection = computerplay();
+console.log(playRound(playerSelection, computerSelection));
 
