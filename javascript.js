81d1b732da59e4cb474b21b3a334fd962d379fbb
Luigi <luigi.drnasin12@gmail.com>
2022-03-04 08:49:32 +0100

    First function

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function computerplay(){
+    var computer = ["Rock", "Paper", "Scissors"];
+    var computerChoice = player[Math.floor(Math.random() * player.length)];
+    return computerChoice;
+}
+
+function game (playerSelection, computerSelection) {
+    
+}
+
+
+
